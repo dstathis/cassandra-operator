40c1d76de8c39d6fd5158ae9dcf9075b84646ae6
Dylan Stephano-Shachter <dylan@theone.ninja>
2021-06-16 19:28:55 +0000

    flake8

--- integration.py
+++ integration.py
@@ -14,10 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import logging
 import pytest
-import unittest
-from juju import loop
 from juju.model import Model
 
 
